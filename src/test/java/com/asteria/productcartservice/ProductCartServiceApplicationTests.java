b9d8c4af1dfbd3a13ae067d6649976a3f9cd3c0c
James Gwatkin <james.gwatkin@asteriasolutions.co.uk>
2024-05-30 12:45:02 -0300

    Initial Commit

---
+++ src/test/java/com/asteria/productcartservice/ProductCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asteria.productcartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
